825def537bf58aae9e43f81bb2cc7b3582f3078d
Emiliano Steinvorth <75647452+Steinvorth@users.noreply.github.com>
2024-04-03 15:55:48 -0600

    Boton Ayuda

--- ProyectoFinal/ProyectoFinal/ProyectoFinal/Navigation/MenuPrincipal.xaml.cs
+++ ProyectoFinal/ProyectoFinal/ProyectoFinal/Navigation/MenuPrincipal.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -19,6 +19,13 @@ namespace ProyectoFinal.Navigation
             comida_cong.Clicked += comida_cong_Clicked;
             carnes.Clicked += carnes_ClickedAsync;
             log_out.Clicked += log_out_Clicked;
+            help.Clicked += help_ClickedAsync;
+        }
+
+        private async void help_ClickedAsync(object sender, EventArgs e)
+        {
+            string url = $"https://www.youtube.com/watch?v=eWULmSLfu3E&ab_channel=GOALPROJECT";
+            await Launcher.OpenAsync(url);
         }
 
         //Navigation a Carnes Page
@@ -36,7 +43,7 @@ namespace ProyectoFinal.Navigation
         //Boton de LogOut, Este boton devuelve al LOGIN.
         private void log_out_Clicked(object sender, EventArgs e)
         {
-            Application.Current.MainPage = new NavigationPage(new MainPage());
+            Application.Current.MainPage = new NavigationPage(new MainPage()); //devuelve a la pagina principal.
         }
     }
 }
